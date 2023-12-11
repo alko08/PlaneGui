79cff20e12f33180fb359c0c9bdce411f14875ae
Alexander Koppel <32113635+alko08@users.noreply.github.com>
2023-12-11 03:25:38 -0500

    Update GameController.cs

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,9 +6,11 @@ public class GameController : MonoBehaviour
 {
     public List <PlaneMove> planes;
     public Transform mainCamera, jetSpawn, propSpawn, heliSpawn;
-    public bool lookAtPlane = false, followPlane = false;
     public GameObject jetPrefab, propPrefab, heliPrefab;
-    public int curPlane = 0;
+    public TMPro.TextMeshProUGUI count, up, down;
+
+    private bool lookAtPlane = true, followPlane = false, firstPerson = false;
+    private int curPlane = 0;
     
 
 
@@ -23,56 +25,89 @@ public class GameController : MonoBehaviour
     // }
 
     void Update() {
-        if (lookAtPlane || followPlane) {
+        if (lookAtPlane || followPlane || firstPerson) {
             if (followPlane) {
-                mainCamera.transform.position = planes[curPlane].gameObject.transform.position + new Vector3(10, 10, 10);
+                mainCamera.transform.position = planes[curPlane].gameObject.transform.GetChild(0).position;
+            } else if (firstPerson) {
+                Vector3 followAdjust = -10 * (planes[curPlane].transform.forward) + new Vector3(0f, 1f, 0f);
+                mainCamera.transform.position = planes[curPlane].gameObject.transform.position + followAdjust;
             }
 
             mainCamera.LookAt(planes[curPlane].gameObject.transform);
         }
+
+        count.text = "" + curPlane;
+
+        if (planes[curPlane].isHeli) {
+            up.text = "Forward";
+            down.text = "Back";
+        } else {
+            up.text = "Up";
+            down.text = "Down";
+        }
     }
 
     public void speedUpPlane() {
-        planes[curPlane].speed += 5;
+        if (planes[curPlane].launched) {
+            planes[curPlane].speed += 5;
+        }
     }
 
     public void slowDownPlane() {
-        planes[curPlane].speed -= 5;
+        if (planes[curPlane].launched) {
+            planes[curPlane].speed -= 5;
+            if (!planes[curPlane].isHeli && planes[curPlane].speed < 0) {
+                planes[curPlane].speed = 0;
+            }
+        }
     }
 
-    public void launchPlanes() {
+    public void launchPlane() {
         planes[curPlane].launch();
     }
 
-    public void turnRightPlanes() {
+    public void turnRight() {
         planes[curPlane].turnRight();
     }
 
-    public void turnLeftPlanes() {
+    public void turnLeft() {
         planes[curPlane].turnLeft();
     }
 
     public void lookAtPlaneButton() {
         lookAtPlane = !lookAtPlane;
+        followPlane = false;
+        firstPerson = false;
     }
 
     public void followPlaneButton() {
         followPlane = !followPlane;
+        lookAtPlane = false;
+        firstPerson = false;
+    }
+
+    public void firstPersonButton() {
+        firstPerson = !firstPerson;
+        lookAtPlane = false;
+        followPlane = false;
     }
 
     public void createJet() {
         GameObject jet = Instantiate(jetPrefab, jetSpawn.position, jetSpawn.rotation);
         planes.Add(jet.GetComponent<PlaneMove>());
+        curPlane = planes.Count - 1;
     }
     
     public void createProp() {
         GameObject prop = Instantiate(propPrefab, propSpawn.position, propSpawn.rotation);
         planes.Add(prop.GetComponent<PlaneMove>());
+        curPlane = planes.Count - 1;
     }
     
     public void createHeli() {
         GameObject heli = Instantiate(heliPrefab, heliSpawn.position, heliSpawn.rotation);
         planes.Add(heli.GetComponent<PlaneMove>());
+        curPlane = planes.Count - 1;
     }
 
     public void nextPlane() {
@@ -80,5 +115,24 @@ public class GameController : MonoBehaviour
         if (curPlane >= planes.Count) {
             curPlane = 0;
         }
+    }
+
+    public void backPlane() {
+        curPlane--;
+        if (curPlane < 0) {
+            curPlane = planes.Count -1;
+        }
+    }
+
+    public void turnUp() {
+        planes[curPlane].moveUp();
+    }
+
+    public void turnDown() {
+        planes[curPlane].moveDown();
+    }
+
+    public void reset() {
+        planes[curPlane].reset();
     }
 }
