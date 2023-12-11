f4e31431957eaa9757c2e5ea82f5c959ead5df09
Alexander Koppel <32113635+alko08@users.noreply.github.com>
2023-12-10 20:59:31 -0500

    Plane Prefab Test

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,76 +4,47 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    public GameObject[] planes;
+    public PlaneMove[] planes;
     public Transform mainCamera;
-    public bool lookAtPlane = false, followPlane = false, turnLeft = false;
+    public bool lookAtPlane = false, followPlane = false;
 
-    private bool launched = false;
-    private int tick = 0;
-    private float smooth = 2.0f;
-    private List<Quaternion> targets;
 
     // Start is called before the first frame update
-    void Start() {
-        targets = new List<Quaternion>();
-        for (int i = 0; i < planes.Length; i++) {
-            targets.Add(planes[i].transform.rotation);
-        }
-    }
+    // void Start() {
+
+    // }
 
     // Update is called once per frame
-    void FixedUpdate() {
-        if (launched) {
-            foreach (GameObject plane in planes) {
-                plane.GetComponent<Rigidbody>().AddForce(plane.transform.forward * 30);
-                if (100 < tick && tick < 300) {
-                    plane.GetComponent<Rigidbody>().AddForce(transform.up * 12);
-                    targets[0] = Quaternion.Euler(-15, targets[0].eulerAngles.y, targets[0].eulerAngles.z);
-                } else {
-                    plane.GetComponent<Rigidbody>().AddForce(transform.up * 9.8f);
-                    targets[0] = Quaternion.Euler(0, targets[0].eulerAngles.y, targets[0].eulerAngles.z);
-                }
-
-                
-            }
-            tick++;
-            // Debug.Log(tick);       
-        }
-
-        if (lookAtPlane || followPlane) {
-            mainCamera.LookAt(planes[0].transform);
-
-            if (followPlane) {
-                mainCamera.transform.position = planes[0].transform.position + new Vector3(10, 10, 10);
-            }
-        }
-    }
+    // void FixedUpdate() {
+       
+    // }
 
     void Update() {
-        // Dampen towards the target rotation
-        for (int i = 0; i < planes.Length; i++) {
-            float zAngle = 0;
-            if (planes[i].transform.eulerAngles.y < targets[i].eulerAngles.y - 10 && planes[i].transform.position.y > 10) {
-                zAngle = -45;
-            } else if (planes[i].transform.eulerAngles.y > targets[i].eulerAngles.y + 10 && planes[i].transform.position.y > 10) {
-                zAngle = 45;
+        if (lookAtPlane || followPlane) {
+            if (followPlane) {
+                mainCamera.transform.position = planes[0].gameObject.transform.position + new Vector3(10, 10, 10);
             }
-            targets[i] = Quaternion.Euler(targets[i].eulerAngles.x, targets[i].eulerAngles.y, zAngle);
 
-            planes[i].transform.rotation = Quaternion.Slerp(planes[i].transform.rotation, targets[i],  Time.deltaTime * smooth);
+            mainCamera.LookAt(planes[0].gameObject.transform);
         }
     }
 
     public void launchPlanes() {
-        launched = true;
+        for (int i = 0; i < planes.Length; i++) {
+            planes[i].launched = true;
+        }
     }
 
     public void turnRightPlanes() {
-        targets[0] = Quaternion.Euler(targets[0].eulerAngles.x, targets[0].eulerAngles.y + 90, targets[0].eulerAngles.z);
+        for (int i = 0; i < planes.Length; i++) {
+            planes[i].turnRight();
+        }
     }
 
     public void turnLeftPlanes() {
-        targets[0] = Quaternion.Euler(targets[0].eulerAngles.x, targets[0].eulerAngles.y - 90, targets[0].eulerAngles.z);
+        for (int i = 0; i < planes.Length; i++) {
+            planes[i].turnLeft();
+        }
     }
 
     public void lookAtPlaneButton() {
---
+++ Assets/Scripts/PlaneMove.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneMove : MonoBehaviour
+{
+    public float speed = 30f, turnSpeed = 2f;
+    public bool launched = false;
+
+    private Rigidbody rb;
+    private Quaternion target;
+    private Transform trans;
+    private int tick = 0;
+
+    // Start is called before the first frame update
+    void Start() {
+        rb = gameObject.GetComponent<Rigidbody>();
+        trans = gameObject.transform;
+        target = trans.rotation;
+    }
+
+    // FixedUpdate is called once per tick
+    void FixedUpdate() {
+        if (launched) {
+            rb.AddForce(trans.forward * 30);
+            if (100 < tick && tick < 300) {
+                rb.AddForce(transform.up * 12);
+                target = Quaternion.Euler(-15, target.eulerAngles.y, target.eulerAngles.z);
+            } else {
+                rb.AddForce(transform.up * 9.8f);
+                target = Quaternion.Euler(0, target.eulerAngles.y, target.eulerAngles.z);
+            }
+
+            tick++;
+            // Debug.Log(tick);       
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // Dampen towards the target rotation
+        float zAngle = 0;
+        if (trans.eulerAngles.y < target.eulerAngles.y - 10 && trans.position.y > 10) {
+            zAngle = -45;
+        } else if (trans.eulerAngles.y > target.eulerAngles.y + 10 && trans.position.y > 10) {
+            zAngle = 45;
+        }
+        target = Quaternion.Euler(target.eulerAngles.x, target.eulerAngles.y, zAngle);
+        trans.rotation = Quaternion.Slerp(trans.rotation, target,  Time.deltaTime * turnSpeed);
+    }
+
+     public void turnRight() {
+        target = Quaternion.Euler(target.eulerAngles.x, target.eulerAngles.y + 90, target.eulerAngles.z);
+    }
+
+    public void turnLeft() {
+        target = Quaternion.Euler(target.eulerAngles.x, target.eulerAngles.y - 90, target.eulerAngles.z);
+    }
+}
