e670c9030f2491ec4d7a8900d15c909a9da1e546
Alexander Koppel <32113635+alko08@users.noreply.github.com>
2023-12-11 03:25:09 -0500

    Basically final Physics Build

--- Assets/Scripts/PlaneMove.cs
+++ Assets/Scripts/PlaneMove.cs
@@ -5,20 +5,21 @@ using UnityEngine;
 public class PlaneMove : MonoBehaviour
 {
     public float speed = 30f, turnSpeed = 2f;
-    public bool isHeli = false;
+    public bool isHeli = false, launched = false;
     public Vector3 targetLaunch;
 
     private Rigidbody rb;
     private Quaternion target;
     private Transform trans;
     private int tick = 0;
-    private bool launched = false, starting = false;
+    private bool starting = false;
+    private float targetX = 0f;
 
     // Start is called before the first frame update
     void Start() {
         rb = gameObject.GetComponent<Rigidbody>();
         trans = transform;
-        target = Quaternion.Euler(trans.eulerAngles.x, trans.eulerAngles.y, target.eulerAngles.z);
+        target = Quaternion.Euler(0, trans.eulerAngles.y, 0);        
     }
 
     // FixedUpdate is called once per tick
@@ -28,16 +29,31 @@ public class PlaneMove : MonoBehaviour
                 rb.AddForce(trans.forward * speed);
                 if (100 < tick && tick < 300) {
                     rb.AddForce(transform.up * 12);
+                    targetX = -15;
                     target = Quaternion.Euler(-15, target.eulerAngles.y, target.eulerAngles.z);
+                } else if (tick == 300) {
+                    targetX = 0;
                 } else {
-                    rb.AddForce(transform.up * 9.8f);
-                    target = Quaternion.Euler(0, target.eulerAngles.y, target.eulerAngles.z);
+                    if (speed != 0) {
+                        rb.AddForce(transform.up * 9.8f);
+                    }
+                    target = Quaternion.Euler(targetX, target.eulerAngles.y, target.eulerAngles.z);
                 }
             } else {
-                if (100 < tick && tick < 300) {
-                    rb.AddForce(transform.up * speed);
-                } else {
+                if (tick <= 100) {
                     rb.AddForce(transform.up * 9.8f);
+                } else if (100 < tick && tick < 300) {
+                    rb.AddForce(transform.up * speed);
+                } else if (tick == 300) {
+                    targetX = 30f;
+                } else {
+                    target = Quaternion.Euler(targetX, target.eulerAngles.y, target.eulerAngles.z);
+                    rb.AddForce(transform.up * speed);
+                }
+
+                if (tick > 300) {
+                    Rotate rotors = gameObject.transform.GetChild(1).GetChild(3).gameObject.GetComponent<Rotate>();
+                    rotors.speed = 1000 * (speed / 15);
                 }
             }
 
@@ -46,13 +62,22 @@ public class PlaneMove : MonoBehaviour
         }
 
         if (trans.position.x < -500 || trans.position.x > 500 || trans.position.z < -500 || trans.position.z > 500) {
-            Vector3 rotation = Quaternion.LookRotation(-trans.position, Vector3.up).eulerAngles;
-            target = Quaternion.Euler(target.eulerAngles.x, rotation.y, target.eulerAngles.z);
+            if (!isHeli) {
+                Vector3 rotation = Quaternion.LookRotation(-trans.position, Vector3.up).eulerAngles;
+                target = Quaternion.Euler(target.eulerAngles.x, rotation.y, target.eulerAngles.z);
+            } else {
+                Vector3 rotation = Quaternion.LookRotation(-trans.position, trans.up).eulerAngles;
+                target = Quaternion.Euler(target.eulerAngles.x, rotation.y, target.eulerAngles.z);
+            }
         }
     }
 
     // Update is called once per frame
     void Update() {
+        if (trans.position.y >= 10) {
+            starting = false;
+        }
+
         // Dampen towards the target rotation
         float zAngle = 0;
         if (trans.eulerAngles.y < target.eulerAngles.y - 10 && trans.position.y > 10) {
@@ -60,13 +85,14 @@ public class PlaneMove : MonoBehaviour
         } else if (trans.eulerAngles.y > target.eulerAngles.y + 10 && trans.position.y > 10) {
             zAngle = 45;
         }
-        target = Quaternion.Euler(target.eulerAngles.x, target.eulerAngles.y, zAngle);
+        target = Quaternion.Euler(targetX, target.eulerAngles.y, zAngle);
         trans.rotation = Quaternion.Slerp(trans.rotation, target,  Time.deltaTime * turnSpeed);
 
-        if (starting) {
+        if (starting && trans.position.y < 10) {
             trans.position = Vector3.MoveTowards(trans.position, targetLaunch, Time.deltaTime * turnSpeed * 2);
             if (trans.position == targetLaunch) {
-                turnLeft();
+                // turnLeft();
+                target = Quaternion.Euler(0, 0, 0);
                 starting = false;
                 StartCoroutine(readyToLaunch());
             }
@@ -74,17 +100,19 @@ public class PlaneMove : MonoBehaviour
     }
 
     public void turnRight() {
-        target = Quaternion.Euler(target.eulerAngles.x, target.eulerAngles.y + 90, target.eulerAngles.z);
+        target = Quaternion.Euler(targetX, target.eulerAngles.y + 90, target.eulerAngles.z);
     }
 
     public void turnLeft() {
-        target = Quaternion.Euler(target.eulerAngles.x, target.eulerAngles.y - 90, target.eulerAngles.z);
+        target = Quaternion.Euler(targetX, target.eulerAngles.y - 90, target.eulerAngles.z);
     }
 
     public void launch() {
         if (isHeli) {
             StartCoroutine(startHeli());
         } else {
+            Vector3 rotation = Quaternion.LookRotation(targetLaunch-trans.position, Vector3.up).eulerAngles;
+            target = Quaternion.Euler(targetX, rotation.y, target.eulerAngles.z);
             starting = true;
         }
     }
@@ -95,9 +123,57 @@ public class PlaneMove : MonoBehaviour
     }
 
     IEnumerator startHeli() {
-        Rotate rotors = gameObject.transform.GetChild(0).GetChild(3).gameObject.GetComponent<Rotate>();
+        Rotate rotors = gameObject.transform.GetChild(1).GetChild(3).gameObject.GetComponent<Rotate>();
         rotors.rotate = true;
         yield return new WaitUntil(() => rotors.speed == 1000f); 
         launched = true;
+    }
+
+    public void moveUp() {
+        if (launched) {
+            if (isHeli) {
+                targetX += 15;
+                if (targetX > 90) {
+                    targetX = 90;
+                }
+            } else {
+                targetX -= 15;
+                if (targetX < -90) {
+                    targetX = -90;
+                }
+            }
+        }
+    }
+
+    public void moveDown() {
+        if (launched) {
+            if (isHeli) {
+                targetX -= 15;
+                if (targetX < -90) {
+                    targetX = -90;
+                }
+            } else {
+                targetX += 15;
+                if (targetX > 90) {
+                    targetX = 90;
+                }
+            }
+        }
+    }
+
+    public void reset() {
+        tick = 0;
+        targetX = 0;
+        launched = false;
+        starting = false;
+        target = Quaternion.Euler(0f, 90f, 0f);
+        if (isHeli) {
+            target = Quaternion.Euler(0f, 0f, 0f);
+            Rotate rotors = gameObject.transform.GetChild(1).GetChild(3).gameObject.GetComponent<Rotate>();
+            rotors.speed = 0;
+            rotors.rotate = false;
+        }
+        trans.rotation = target;
+        trans.position = targetLaunch;
     }
 }
